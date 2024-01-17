7a697df4c30d5dad222d2a6e26bab76456330918
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-17 21:14:22 +0530

    chore: added styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,49 @@
+<script>
+	import ButtonSection from './sections/ButtonSection.svelte';
+</script>
+
 <main class="Home">
 	<section class="Home__hero">
 		<h1 class="Home__hero--title">Crisp</h1>
 		<p class="Home__hero--subtitle">A collection useful of SCSS utility classes</p>
+		<a
+			class="CrispButton"
+			href="https://github.com/ABSanthosh/Crisp"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Github
+		</a>
 	</section>
+	<ButtonSection />
 </main>
 
 <style lang="scss">
 	.Home {
-		@include box();
-		overflow: auto;
-		padding-top: 64px;
-		background-image: radial-gradient(#ddd 1px, transparent 0),
-			radial-gradient(#ddd 1px, transparent 0);
-		background-position:
-			0 0,
-			25px 25px;
-		background-size: 50px 50px;
+		gap: 96px;
+		padding: 0 24px;
+		margin: 100px 0 400px 0;
+		max-width: 618px;
+		@include box($height: auto);
 		@include make-flex($just: flex-start);
 
 		&__hero {
-			@include box($height: 50%);
 			gap: 30px;
+			@include box($height: auto);
 			@include make-flex($dir: column, $just: center, $align: center);
 
 			&--title {
-				font-family: 'Grape Nuts', cursive;
-				font-size: 180px;
-				font-weight: 900;
 				color: #4ebf59;
+				font-size: 150px;
+				font-weight: 500;
+				text-align: center;
 				text-decoration: none;
+				font-family: 'Grape Nuts', cursive;
 			}
 
 			&--subtitle {
-				font-size: 25px;
-				color: #6a6a6a;
+				font-size: 19px;
+				text-align: center;
 			}
 		}
 	}
